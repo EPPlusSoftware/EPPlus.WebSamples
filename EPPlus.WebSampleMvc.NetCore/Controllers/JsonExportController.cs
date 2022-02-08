288edd1ea68b6254f93edb5694516460da2b1a1c
JanKallman <jan.kallman@epplussoftware.com>
2022-02-08 09:41:55 +0100

    Small changes

--- EPPlus.WebSampleMvc.NetCore/Controllers/JsonExportController.cs
+++ EPPlus.WebSampleMvc.NetCore/Controllers/JsonExportController.cs
@@ -30,12 +30,12 @@ namespace EPPlus.WebSampleMvc.NetCore.Controllers
                 {
                     Delimiter = ';',
                     Culture = CultureInfo.InvariantCulture,
-                    DataTypes = new eDataTypes[] { eDataTypes.DateTime, eDataTypes.Number, eDataTypes.Number, eDataTypes.Number }
+                    DataTypes = new eDataTypes[] { eDataTypes.DateTime, eDataTypes.Number, eDataTypes.Number, eDataTypes.Number }                    
                 };
                 var range = await sheet.Cells["A1"].LoadFromTextAsync(csvFileInfo, format);
                 sheet.Cells[range.Start.Row, 1, range.End.Row, 1].Style.Numberformat.Format = "yyyy-MM-dd";
                 sheet.Cells[range.Start.Row, 2, range.End.Row, 5].Style.Numberformat.Format = "#,##0.0000";
-                var jsonData = range.ToJson();
+                var jsonData = range.ToJson(x=>x.AddDataTypesOn = eDataTypeOn.OnColumn);
                 return Json(jsonData);
             }
         }
