226e36d6921b939fa2e00f0041d9515a4bf1a114
Mats Alm <897655+swmal@users.noreply.github.com>
2022-02-10 16:40:29 +0100

    Added new worksheet in Blazor sample

--- EPPlus.WebSampleMvc.NetCore/Controllers/BlazorSamplesController.cs
+++ EPPlus.WebSampleMvc.NetCore/Controllers/BlazorSamplesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +17,14 @@ namespace EPPlus.WebSampleMvc.NetCore.Controllers
         public IActionResult Sample1()
         {
             return View();
+        }
+
+        [HttpGet, Route("/api/fxrates")]
+        public IActionResult FxRates()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"data\\FxRates.json");
+            var json = System.IO.File.ReadAllText(path);
+            return Json(json);
         }
     }
 }
