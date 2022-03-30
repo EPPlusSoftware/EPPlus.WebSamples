62b5fbfd940433d38e75a39a32e9237d7a7898a2
Mats Alm <897655+swmal@users.noreply.github.com>
2022-03-30 17:31:28 +0200

    WIP: more work on html sample 6

--- EPPlus.WebSampleMvc.NetCore/Models/HtmlExport/ExportRanges6Model.cs
+++ EPPlus.WebSampleMvc.NetCore/Models/HtmlExport/ExportRanges6Model.cs
@@ -1,4 +1,5 @@
 ﻿using OfficeOpenXml;
+using OfficeOpenXml.Export.HtmlExport;
 using System;
 using System.IO;
 
@@ -11,8 +12,13 @@ namespace EPPlus.WebSampleMvc.NetCore.Models.HtmlExport
             using(var package = new ExcelPackage(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"data\\SwedishGeography.xlsx")))
             {
                 var sheet = package.Workbook.Worksheets[0];
-                var exporter = package.Workbook.CreateHtmlExporter(sheet.Cells["A1:D10"], sheet.Cells["G1:J10"]);
+                var sheet2 = package.Workbook.Worksheets[1];
+                var table = package.Workbook.Worksheets[2].Tables["Municipalities"];
+                var exporter = package.Workbook.CreateHtmlExporter(sheet.Cells["A1:D10"], sheet.Cells["G1:J10"], sheet2.Cells["A1:E73"], table.Range);
+                exporter.Settings.Pictures.Include = ePictureInclude.Include;
                 exporter.Settings.Minify = false;
+                // all hyperlinks should open in a new browser window.
+                exporter.Settings.HyperlinkTarget = "_blank";
                 Html1 = exporter.GetHtmlString(0, x =>
                 {
                     x.TableId = "cities-table";
@@ -23,6 +29,19 @@ namespace EPPlus.WebSampleMvc.NetCore.Models.HtmlExport
                     x.TableId = "lakes-table";
                     x.Accessibility.TableSettings.AriaLabel = "Largest lakes in Sweden";
                 });
+                Html3 = exporter.GetHtmlString(2, x =>
+                {
+                    x.TableId = "kings-table";
+                    x.Accessibility.TableSettings.AriaLabel = "Some kings in Sweden";
+                });
+                Html4 = exporter.GetHtmlString(3, x =>
+                {
+                    x.TableId = "municipalities-table";
+                    x.Accessibility.TableSettings.AriaLabel = "Swedish municipalities";
+                    // Add bootstrap table classes
+                    x.AdditionalTableClassNames.Add("table");
+                    x.AdditionalTableClassNames.Add("table-sm");
+                });
                 Css = exporter.GetCssString();
             }
         }
@@ -32,5 +51,9 @@ namespace EPPlus.WebSampleMvc.NetCore.Models.HtmlExport
         public string Html1 { get; set; }
 
         public string Html2 { get; set; }
+
+        public string Html3 { get; set; }
+
+        public string Html4 { get; set; }
     }
 }
